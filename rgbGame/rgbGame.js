8d6c1bb877c2c3d6be455b63c870eb80705810d4
Cybnew <mathewrupp@gmail.com>
2019-02-13 22:01:56 -0800

    started working on color picking game

---
+++ rgbGame/rgbGame.js
@@ -0,0 +1,50 @@
+var colors = generateColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var guess = document.getElementById("guess");
+var h1 = document.querySelector("h1");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function() {
+    var clickedColor = this.style.backgroundColor;
+    if (clickedColor === pickedColor) {
+      guess.textContent = "Correct!";
+      changeColors(pickedColor);
+      h1.style.backgroundColor = pickedColor;
+    } else {
+      this.style.backgroundColor = "#232323";
+      guess.textContent = "Wrong";
+    }
+  });
+}
+
+var changeColors = color => {
+  for (i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+};
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateColors(num) {
+  arr = [];
+  for (i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  redValue = Math.floor(Math.random() * 255 + 1);
+  blueValue = Math.floor(Math.random() * 255 + 1);
+  greenValue = Math.floor(Math.random() * 255 + 1);
+  return "rgb(" + redValue + ", " + blueValue + ", " + greenValue + ")";
+}
